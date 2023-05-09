d53238a709bc68627216e281bde7f52d68a84948
alyatamzila <132926644+alyatamzila@users.noreply.github.com>
2023-05-09 10:16:11 +0700

    Add files via upload

---
+++ app/Http/Controllers/RouteController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RouteController extends Controller
+{
+    public function profile($profileId) {
+        return "This is Profile from Controller, profile id: ".$profileId;
+    }
+}
